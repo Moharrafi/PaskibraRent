cd7316fd048e5b1b8c01ea3297121c12112cf395
Moharrafi <mohamadarraafi@gmail.com>
2026-03-02 19:27:39 +0700

    debug: expose startup errors in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-const app = require('../server/index');
+let app;
+try {
+    app = require('../server/index');
+} catch (e) {
+    // If startup fails, return a JSON error with the details so we can debug
+    app = (req, res) => {
+        res.status(500).json({
+            startup_error: e.message,
+            stack: e.stack,
+            code: e.code
+        });
+    };
+}
 
 module.exports = app;
